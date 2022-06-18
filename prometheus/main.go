ccbc1eddfc9fc997fae2c3cf8ad9f743eb2db9b8
Daniel <daniel.agostinho@hotmail.com>
2022-06-18 16:29:20 -0300

    Trabalhando com Counter

--- prometheus/main.go
+++ prometheus/main.go
@@ -16,15 +16,28 @@ var onlineUsers = prometheus.NewGauge(prometheus.GaugeOpts{
 	},
 })
 
+var httpRequestTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "goapp_http_requests_total",
+	Help: "Count of all HTTP request for goapp",
+}, []string{})
+
 func main() {
 	r := prometheus.NewRegistry()
 	r.MustRegister(onlineUsers)
+	r.MustRegister(httpRequestTotal)
 
 	go func() {
 		for {
 			onlineUsers.Set(float64(rand.Intn(2000)))
 		}
 	}()
+
+	home := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello Full Cycle"))
+	})
+
+	http.Handle("/", promhttp.InstrumentHandlerCounter(httpRequestTotal, home))
 
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 	log.Fatal(http.ListenAndServe(":8181", nil))
